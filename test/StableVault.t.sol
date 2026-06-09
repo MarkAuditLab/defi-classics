5b28e73758b9033048a5cea3b1314763c6be68c4
MarkAuditLab <001jumatov@gmail.com>
2026-06-09 13:07:40 +0300

    Update StableVault.t.sol

--- test/StableVault.t.sol
+++ test/StableVault.t.sol
@@ -1,15 +1,11 @@
-/ SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
 import "forge-std/Test.sol";
-import "../src/StableVault.sol";
+import "../StableVault.sol"; 
 
 contract StableVaultTest is Test {
     StableVault vault;
-
-    function setUp() public {
-        // Заглушка, позже добавим реальный адрес токена
-    }
 
     function test_Example() public {
         assertTrue(true);
