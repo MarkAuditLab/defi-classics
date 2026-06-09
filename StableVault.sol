0bf74b521f0d1c0ca398870f1f1f5c38a952a6f4
MarkAuditLab <001jumatov@gmail.com>
2026-06-09 12:48:30 +0300

    Update StableVault.sol

--- StableVault.sol
+++ StableVault.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "forge-std/Test.sol";
+import "../src/StableVault.sol"; 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title StableVault
